7c89b9e165712fdacf0e5db9a009c6c1f0748e4a
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-11 15:43:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title
+  "SELECT series.title
   FROM characters
   INNER JOIN series
   ON characters.series_id = series.id
