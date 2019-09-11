cd642bdc2a604f06945eea3f10189b75643fa111
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-11 15:40:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,21 +14,21 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species)
-  FROM characters
-  GROUP BY species
-  ORDER BY COUNT(species) DESC
+  "SELECT species, COUNT(species) 
+  FROM characters 
+  GROUP BY species 
+  ORDER BY COUNT(species) DESC 
   LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  ""
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  ""
 end
